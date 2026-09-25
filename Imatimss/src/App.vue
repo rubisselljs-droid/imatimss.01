b6f101d2853e0f46b5200ca2a76b40d3987cb997
DesarrolloCDI <desarrollocdiyucatan@gmail.com>
2026-09-25 08:38:40 -0600

    nuevos componentes integrados

---
+++ Imatimss/src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+import DatosCard from './components/datosCard.vue';
+//import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <DatosCard />
+</template>
---
+++ Imatimss/src/components/datosCard.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue'
+import { FlLaptopPerson } from '@kalimahapps/vue-icons';
+
+</script>
+
+<template>
+   <div class="flex   border-2 border-sky-950 rounded-lg font-sans w-2xs">
+      <div>
+         <h1 class="text-neutral font-bold">TOTAL DE REGISTROS</h1>
+         <span class="flex justify-center items-center border-neutral border-2 w-10 h-10"><FlLaptopPerson/></span>
+      </div>
+      <div class="flex">
+        <span class="font-bold">1,000</span> 
+      </div>
+     
+   </div>
+</template>
