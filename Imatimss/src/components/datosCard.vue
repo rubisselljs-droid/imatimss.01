c799846781ec973ad2bb61059a065481a7d85baa
DesarrolloCDI <desarrollocdiyucatan@gmail.com>
2026-09-25 08:46:18 -0600

    Subir

--- Imatimss/src/components/datosCard.vue
+++ Imatimss/src/components/datosCard.vue
@@ -5,11 +5,14 @@ import { FlLaptopPerson } from '@kalimahapps/vue-icons';
 </script>
 
 <template>
-   <div class="flex   border-2 border-sky-950 rounded-lg font-sans w-2xs">
-      <div>
+   <div class="border-2 border-sky-950 rounded-lg font-sans w-2xs m-2">
+
+      <div class="grid justify-center grid-">
          <h1 class="text-neutral font-bold">TOTAL DE REGISTROS</h1>
          <span class="flex justify-center items-center border-neutral border-2 w-10 h-10"><FlLaptopPerson/></span>
       </div>
+
+
       <div class="flex">
         <span class="font-bold">1,000</span> 
       </div>
